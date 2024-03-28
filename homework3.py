f8dbbfe43b35252db9818851d6226e0173d5a0d6
Oval-174 <164682443+Oval-174@users.noreply.github.com>
2024-03-28 15:59:56 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+print("Введите строку из нескольких слов: ")
+my_string = input()
+print("Длина строки: ", len(my_string), " символов")
+print("Строка в верхнем регистре: ", my_string.upper())
+print("Строка в нижнем регистре : ", my_string.lower())
+print("Строка без пробелов      : ", my_string.replace(" ", ""))
+print("Первый символ строки:    ", my_string[0])
+print("Последний символ строки: ", my_string[-1])
