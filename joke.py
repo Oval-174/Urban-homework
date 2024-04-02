d7876b27f489c1c79150f439c5835340ccb33998
Veniamin Orlenko <vorlenko@d2k.ru>
2024-04-02 17:51:50 +0300

    Tic-tac-toe

--- joke.py
+++ joke.py
@@ -6,7 +6,10 @@ if year_of_birth_text.isnumeric(): year_of_birth = int(year_of_birth_text)
 text_for_answer = " лет", " год", " года", " года", " года", " лет", " лет", " лет", " лет", " лет"
 age = current_year - year_of_birth
 age_help = age % 10
-if 10 < age < 15 : age_help = 0
+if 10 < age < 15:
+    age_help = 0
 print("Здравствуйте, ", name, "!")
-if age < 5 | age > 100 : print("Хорошая  шутка:")
-if age >4 : print("В этом году Вам ", age, text_for_answer[age_help], '?')
+if age < 5 | age > 100:
+    print("Хорошая  шутка:")
+if age > 4:
+    print("В этом году Вам ", age, text_for_answer[age_help], '?')
