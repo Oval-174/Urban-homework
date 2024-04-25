c982e9ecc2cca7137d577f1faf49f8d63aa7325b
Veniamin Orlenko <vorlenko@d2k.ru>
2024-04-25 17:02:47 +0300

    Practice 1 new

---
+++ nim_engine_new.py
@@ -0,0 +1,32 @@
+from random import randint
+
+MAX_BUNCHES = 5
+MAX_BUNCHES_SIZE = 20
+_holder = {}
+
+
+def put_stones():
+    print('NEW GAME')
+    global _holder
+    _holder = {}
+    for i in range(1, MAX_BUNCHES + 1):
+        _holder[i] = randint(1, MAX_BUNCHES_SIZE)
+
+
+def take_from_bunch(position, quantity):
+    if position in _holder:
+        _holder[position] -= quantity
+        if _holder[position] < 0:
+            _holder[position] = 0
+
+
+def get_bunches():
+    res = []
+    for key in sorted(_holder.keys()):
+        res.append(_holder[key])
+    return res
+
+
+def is_game_over():
+    return sum(_holder.values()) == 0
+
