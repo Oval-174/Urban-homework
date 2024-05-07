fa2c9b475b0e51ac1860628196b99b304208a3d2
Veniamin Orlenko <vorlenko@d2k.ru>
2024-05-07 16:09:59 +0300

    module2hard

---
+++ module2hard.py
@@ -0,0 +1,9 @@
+while True:
+    num = int(input('Введите число "замок": '))
+    if num == 0: break
+    key = ''
+    for i in range(1, num + 1):
+        for j in range(i + 1, num):
+            if num % (i + j) == 0:
+                key = key + str(i) + str(j)
+    print('         Число "ключ": ', key)
