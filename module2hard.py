3653eea404f6736c1785c8e8577e8bd855d87f4c
Veniamin Orlenko <vorlenko@d2k.ru>
2024-05-07 16:18:40 +0300

    module2hard new

--- module2hard.py
+++ module2hard.py
@@ -1,6 +1,7 @@
 while True:
     num = int(input('Введите число "замок": '))
-    if num == 0: break
+    if num < 3:
+        break
     key = ''
     for i in range(1, num + 1):
         for j in range(i + 1, num):
