c3e7f9deb7143aebff38a5b2161ed0029cc7ec0b
Veniamin Orlenko <vorlenko@d2k.ru>
2024-05-07 14:24:50 +0300

    module1hard

--- journal_score.py
+++ journal_score.py
@@ -4,6 +4,6 @@ print(type(students), students)
 list_students = list(sorted(students))
 print(list_students)
 jornal_score = {}
-for i in range (0, len(list_students)):
+for i in range(0, len(list_students)):
     jornal_score[list_students[i]] = round(sum(grades[i]) / len(grades[i]), 2)
 print(type(jornal_score), jornal_score)
